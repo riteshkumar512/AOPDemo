6da25db99e5cf73fd1853947f3a8a50340a2ca84
Ritesh Kumar <riteshkumar051204@gmail.com>
2026-09-19 13:44:34 +0530

    dto

---
+++ src/main/java/in/kumar/AOPDemo/dto/Student.java
@@ -0,0 +1,23 @@
+package in.kumar.AOPDemo.dto;
+
+public class Student {
+    private String name;
+    private int age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
